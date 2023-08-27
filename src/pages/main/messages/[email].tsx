67fd7886e409205258ffb005a060aac25f660789
Stefan283 <iosifstefan220@gmail.com>
2023-08-27 10:43:14 +0300

    media queries

--- src/pages/main/messages/[email].tsx
+++ src/pages/main/messages/[email].tsx
@@ -252,7 +252,7 @@ export default function Messages({ messagesData, avatar, params, username, hasSe
                            email={user?.email || ''} emailSend={params || ''} />
                      ) : (
                         <>
-                           <div className='w-full bg relative overflow-auto flex items-center justify-start flex-col-reverse py-2 pl-10 h-full mobile:h-[calc(100vh-280px)]'
+                           <div className='w-full bg relative overflow-auto flex items-center justify-start flex-col-reverse py-2 pl-10 h-full mobile:h-[calc(100vh-235px)]'
                               ref={scrollRef} style={{ backgroundImage: `url(${bg})` }}
                            >
                               {seen && messages[0]?.email === user?.email && (
