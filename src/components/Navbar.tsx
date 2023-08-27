05c393e1fcf154a49a5eee43a1c8b0375ab3954d
Stefan283 <iosifstefan220@gmail.com>
2023-08-27 21:41:42 +0300

    main

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -156,7 +156,7 @@ export default function Navbar() {
                 </Toolbar>
               </AppBar>
             </Paper>
-            <div style={{ position: 'fixed', left: 0, bottom: 0, zIndex: 20, width: '100vw', height: '56px', backgroundColor: 'red' }}>
+            <div style={{ position: 'sticky', left: 0, bottom: 0, zIndex: 20, width: '100vw', height: '56px', backgroundColor: 'red' }}>
               <BottomNavigation
                 showLabels
               >
