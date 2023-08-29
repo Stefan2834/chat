88a3c3cf84e225e50d36e1c9d42a252272004ba9
Stefan283 <iosifstefan220@gmail.com>
2023-08-29 21:26:16 +0300

    glitch

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Chat-app</title>
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </head>
--- src/pages/main/test.tsx
+++ src/pages/main/test.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-export default function Test() {
-  return (
-    <div>test</div>
-  )
-}
+const ProgressBar = () => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+
+  useEffect(() => {
+    const startLoadTime = window.performance.timing.navigationStart;
+    const pageLoadTime = window.performance.timing.loadEventEnd - startLoadTime;
+
+    const intervalId = setInterval(() => {
+      const currentTime = Date.now() - startLoadTime;
+      const currentProgress = (100 * currentTime) / pageLoadTime;
+      setLoadingProgress(Math.min(currentProgress, 100));
+
+      if (currentTime >= pageLoadTime) {
+        clearInterval(intervalId);
+        setLoadingProgress(100);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div className="progress-bar" style={{ width: `${loadingProgress}%`, height:'30px', backgroundColor:'red' }} />;
+};
+
+export default ProgressBar;
