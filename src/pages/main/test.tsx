55722fa824f483af7e68c8eea906d192e91fc9a1
Stefan283 <iosifstefan220@gmail.com>
2023-08-30 16:28:22 +0300

    not fixed

--- src/pages/main/test.tsx
+++ src/pages/main/test.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
 import Picker from '@emoji-mart/react'
 import data from '@emoji-mart/data'
 
 
 const ProgressBar = () => {
   return (
-    <Picker data={data}
-      theme={'light'}
-      autoFocus={true}
-    />
+    <div className='w-full bg-red-400'>
+
+      <Picker data={data}
+      />
+    </div>
   )
 
 };
