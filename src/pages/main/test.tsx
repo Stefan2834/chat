47c20e2fb251f925e9f9a2aa433eaf82f84ab1ea
Stefan283 <iosifstefan220@gmail.com>
2023-08-30 17:26:43 +0300

    main

--- src/pages/main/test.tsx
+++ src/pages/main/test.tsx
@@ -1,14 +1,20 @@
-import EmojiPicker from "@emoji-mart/react";
+import React, { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
 
+const EmojiPickerComponent = () => {
+  const [chosenEmoji, setChosenEmoji] = useState<any>(null);
 
+  const handleEmojiClick = (event: any, emojiObject: any) => {
+    setChosenEmoji(emojiObject);
+  };
 
-const EmojiPage = () => {
   return (
     <div>
       <h1>Emoji Picker</h1>
-      <EmojiPicker  />
+      <EmojiPicker onEmojiClick={handleEmojiClick} />
+      {chosenEmoji && <p>Chosen Emoji: {chosenEmoji?.emoji}</p>}
     </div>
   );
 };
 
-export default EmojiPage;
+export default EmojiPickerComponent;
