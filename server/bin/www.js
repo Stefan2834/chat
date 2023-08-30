188f144785bb2f8accff3285a6c1794281afc51d
Stefan283 <iosifstefan220@gmail.com>
2023-08-30 18:40:07 +0300

    working perfect

--- server/bin/www.js
+++ server/bin/www.js
@@ -166,7 +166,7 @@ var port = normalizePort(process.env.PORT || '9000');
 app.set('port', port);
 
 server.listen(port, 
-  // '192.168.100.34', 
+  '192.168.100.34', 
   async () => {
   await connect()
   console.log(`Server is on port: ${port}`)
