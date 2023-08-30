327eba0a97346346c9e3d8527db2dd694c21791a
Stefan283 <iosifstefan220@gmail.com>
2023-08-30 17:12:46 +0300

    test

--- src/pages/main/test.tsx
+++ src/pages/main/test.tsx
@@ -1,15 +1,11 @@
 import { FC } from 'react';
-import dynamic from 'next/dynamic';
-
-const DynamicPicker = dynamic(() => import('@emoji-mart/react').then(module => module.default), {
-  ssr: false // Prevent SSR for this component
-});
+import Picker from '@emoji-mart/react';
 
 const EmojiPage: FC = () => {
   return (
     <div>
       <h1>Emoji Picker</h1>
-      <DynamicPicker />
+      <Picker  />
     </div>
   );
 };
