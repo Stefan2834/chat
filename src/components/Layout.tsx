f776ab72957b089e08c76ac30c129bdf1f9b56e6
Stefan283 <iosifstefan220@gmail.com>
2023-09-05 22:13:53 +0300

    main

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 
   return (
     <div className={inter.className}>
-      <Head>
+      {/* <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Chat-app</title>
@@ -28,7 +28,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-      </Head>
+      </Head> */}
       <main>
         {children}
       </main>
